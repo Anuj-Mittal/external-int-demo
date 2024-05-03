f4a2c2af473e37516c0bd1ca2199ab781b48b2a7
Anuj Mittal <anuj.mittal@sprinklr.com>
2024-05-03 19:13:54 +0530

    init

--- src/Layout.js
+++ src/Layout.js
@@ -1,12 +1,16 @@
 import { Flex } from "antd";
-import { useEffect, useRef } from "react";
-import { Card, Col, Row } from "antd";
-import { InputNumber } from "antd";
+import { useEffect, useRef, useState } from "react";
+import { Card, Col, Row, Button, Popover, InputNumber } from "antd";
+import { PhoneTwoTone } from "@ant-design/icons";
 
 let sprComponentsMounted = false;
 
 export const Layout = () => {
+  const [isCtiVisible, setIsCtiVisible] = useState(false);
   const caseDetailSprComponentRef = useRef();
+  const ctiSprComponentRef = useRef();
+  const guidedPathSprComponentRef = useRef();
+
   // 7316716
   useEffect(() => {
     if (!sprComponentsMounted) {
@@ -17,40 +21,92 @@ export const Layout = () => {
         {
           component: "CASE_DETAIL",
           container: document.getElementById("caseDetailContainer"),
-          props: { caseNumber: "" },
+          props: { caseNumber: 7316716 },
         }
       );
+
+      ctiSprComponentRef.current = window.spr.integrations.command("MOUNT", {
+        component: "CTI",
+        container: document.getElementById("ctiContainer"),
+        props: { caseNumber: "" },
+      });
+
+      // guidedPathSprComponentRef.current = window.spr.integrations.command(
+      //   "MOUNT",
+      //   {
+      //     component: "GUIDED_PATH",
+      //     container: document.getElementById("guidedPathContainer"),
+      //     props: {
+      //       actionDetails: {
+      //         action: 51563,
+      //       },
+      //     },
+      //   }
+      // );
     }
   }, []);
 
   return (
-    <Flex
-      align="start"
-      justify="center"
-      style={{ padding: "50px" }}
-      gap="middle"
-    >
-      <Card title="Case Details" bordered={true}>
+    <Flex justify="start" gap="middle" style={{ padding: "40px" }}>
+      <Flex vertical gap="middle" justify="flex-end">
         <div
-          id="caseDetailContainer"
+          id="ctiContainer"
           style={{
-            width: "500px",
-            height: "600px",
+            width: "340px",
+            height: "560px",
+            borderRadius: "10px",
+            overflow: "scroll",
+            opacity: isCtiVisible ? 1 : 0,
           }}
         />
-      </Card>
+        <Button
+          style={{ width: "100px" }}
+          icon={
+            <PhoneTwoTone
+              style={{
+                transform: "scaleX(-1)",
+              }}
+            />
+          }
+          onClick={() => {
+            setIsCtiVisible((v) => !v);
+          }}
+        >
+          Phone
+        </Button>
+      </Flex>
       <div
+        id="caseDetailContainer"
         style={{
-          width: "300px",
+          width: "600px",
+          height: "700px",
         }}
-      >
+      />
+      <Flex vertical gap="middle">
         <InputNumber
           addonBefore="Case Number"
           onChange={(caseNumber) => {
             caseDetailSprComponentRef.current.update({ caseNumber });
           }}
         />
-      </div>
+        <InputNumber
+          addonBefore="Guided Path Id"
+          onChange={(action) => {
+            guidedPathSprComponentRef.current.update({
+              actionDetails: {
+                action,
+              },
+            });
+          }}
+        />
+        <div
+          id="guidedPathContainer"
+          style={{
+            width: "400px",
+            height: "500px",
+          }}
+        />
+      </Flex>
     </Flex>
   );
 };
--- src/index.js
+++ src/index.js
@@ -4,7 +4,8 @@ import App from "./App";
 
 // add external-integrations.js to dom
 const script = document.createElement("script");
-script.src = "https://localhost:8000/_next/static/external-integrations.js";
+script.src =
+  "https://qa6-staticcf.sprinklr.com/apps/integrations/_next/static/external-integrations.js";
 script.async = true;
 script.onload = () => {
   const root = ReactDOM.createRoot(document.getElementById("root"));
